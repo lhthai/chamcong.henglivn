cfa7466616de06ed3abd7acb1d011f8863b207d7
mrsimonslice <mrsimonslice@gmail.com>
2018-07-09 15:40:42 +0700

    Initilize

---
+++ libs/handle.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports.handleFile = function() {
+  var XLSX = require("xlsx");
+  var workbook = XLSX.readFile("./public/upload/file.xlsx", {
+    cellDates: true
+  });
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  const data = XLSX.utils.sheet_to_json(worksheet);
+  const filename = Date.now();
+  for (i = 4; i <= data.length + 3; i++) {
+    if (
+      worksheet["A" + i] != undefined &&
+      worksheet["E" + i] != undefined &&
+      worksheet["G" + i] != undefined &&
+      worksheet["H" + i] != undefined
+    ) {
+      let id = worksheet["A" + i].v;
+      let timein = worksheet["G" + i].v;
+      let timeout = worksheet["H" + i].v;
+      let date = chageDateIfNightShift(worksheet["E" + i].v, timein, timeout);
+
+      let str =
+        id +
+        "\t" +
+        date +
+        " " +
+        timein +
+        "\t" +
+        "2 \t 0 \r\n" +
+        id +
+        "\t" +
+        date +
+        " " +
+        timeout +
+        "\t" +
+        "2 \t 0 \r\n";
+      fs.appendFileSync(`./public/chamcong/${filename}.txt`, str, "utf8");
+    }
+  }
+};
+
+function chageDateIfNightShift(date, timein, timeout) {
+  let d = new Date(date);
+  let newDate = new Date(d);
+  const hourIn = parseInt(timein.slice(0, 2));
+  const hourOut = parseInt(timeout.slice(0, 2));
+
+  if (hourIn > hourOut && hourIn >= 19) {
+    //return true if the time is more than 19:00:00
+    newDate.setDate(d.getDate() + 2);
+    return newDate.toISOString().slice(0, 10);
+  } else {
+    //return false if the time is less than 19:00:00
+    newDate.setDate(d.getDate() + 1);
+    return newDate.toISOString().slice(0, 10);
+  }
+}
---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const port = process.env.PORT || 3000;
+const helmet = require("helmet");
+const compression = require("compression");
+const multer = require("multer");
+const app = new express();
+const path = require("path");
+const handle = require("./libs/handle");
+
+// Configure middleware
+
+// set storage engine
+const storage = multer.diskStorage({
+  destination: "./public/upload/",
+  filename: function(req, file, cb) {
+    cb(null, "file" + path.extname(file.originalname));
+  }
+});
+// init upload
+const upload = multer({
+  storage: storage,
+  fileFilter: function(req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).single("txtInput");
+// CheckFileType function
+function checkFileType(file, cb) {
+  const filetypes = /xlsx/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  if (extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Chỉ sử dụng file excel .xlsx");
+  }
+}
+// configure helmet middleware
+app.use(helmet());
+
+// configure expression middleware
+app.use(compression());
+
+// configure ejs middleware
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.static(path.join(__dirname, "public")));
+
+// setting up routes
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      res.redirect('/');
+    } else {
+      handle.handleFile();
+      res.redirect('/');
+    }
+  });
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+if (app.get("env") === "development") {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(port, () => console.log(`App running on port ${port}`));
